5b36dd7c873479580c9976c4934d8c3c91e8db35
Kyongsik Yun <yunkss@gmail.com>
2024-06-06 07:16:56 -0700

    Update slim-cli.py

--- slim-cli.py
+++ slim-cli.py
@@ -1,6 +1,8 @@
 import argparse
 import logging
 import sys
+import requests
+import re
 
 def setup_logging():
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
