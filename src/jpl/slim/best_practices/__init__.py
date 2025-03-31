1bda583a1bbe175fb0dff12a7af19fd37a9be97e
Rishi Verma <riverma@apache.org>
2025-03-31 15:48:44 -0700

    Remove another ref to governance.py

--- src/jpl/slim/best_practices/__init__.py
+++ src/jpl/slim/best_practices/__init__.py
@@ -8,4 +8,4 @@ that can be applied to repositories.
 from jpl.slim.best_practices.base import BestPractice
 from jpl.slim.best_practices.standard import StandardPractice
 
-__all__ = ["BestPractice", "GovernancePractice", "StandardPractice"]
+__all__ = ["BestPractice", "StandardPractice"]
