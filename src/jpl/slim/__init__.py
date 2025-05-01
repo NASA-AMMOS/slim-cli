b58da21578ba72dcfb0d1f91157f9ada8bf6c585
Kyongsik Yun <yunkss@gmail.com>
2025-05-01 15:27:30 -0700

    automated pypi testing

--- src/jpl/slim/__init__.py
+++ src/jpl/slim/__init__.py
@@ -6,4 +6,4 @@ import importlib.resources
 
 
 PACKAGE_NAME = __name__
-__version__ = VERSION = importlib.resources.files(__name__).joinpath('VERSION.txt').read_text().strip()
+__version__ = "0.0.7"
